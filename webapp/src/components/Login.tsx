cd192c8b5f4adf6b6358c10ee4f2d75348bc45c2
NikoTrif <nikotrif@gmail.com>
2023-12-04 11:20:06 +0100

    TD1a - CSS interface - main finished

--- webapp/src/components/Login.tsx
+++ webapp/src/components/Login.tsx
@@ -1,4 +1,8 @@
 import '../styles/login/login.scss';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 //TD1a
 const Login: React.FC = () => {
@@ -6,12 +10,12 @@ const Login: React.FC = () => {
         <div className='login'>
             <img src={require('../img/login.png')} alt="Login Image" />
             <form>
-                <label htmlFor="email">e-mail</label>
-                <input type="text" name="email" id="email" />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
-                <button>Login</button>
-                <a href="#">SignUp</a>
+                <label htmlFor="email" className='form-label'>e-mail</label>
+                <input type="text" name="email" id="email" className='form-control' />
+                <label htmlFor="password" className='form-label'>Password</label>
+                <input type="password" name="password" id="password" className='form-control' />
+                <button className='btn btn-success'>Login</button>
+                <a href="#" className='link-primary'>SignUp</a>
             </form>
         </div>
     );
