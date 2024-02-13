c9171dcf05ba20f6adc2b928d84b1d2bce47d9c0
NikoTrif <nikotrif@gmail.com>
2024-02-13 18:03:36 +0100

    start

--- webapp/src/components/calendar/MonthlyView.tsx
+++ webapp/src/components/calendar/MonthlyView.tsx
@@ -1,6 +1,32 @@
+import { format } from 'date-fns';
+
 const MonthlyView: React.FC = () => {
+
+
+    const currentDate = new Date();
+
     return ( 
-        <h2>Monthly View</h2>
+        <div>
+            <h2>{format(currentDate, 'MMMM yyyy')}</h2>
+            <div>
+                <button>{"<"}</button>
+                <button>{">"}</button>
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Mon</th>
+                        <th>Tue</th>
+                        <th>Wed</th>
+                        <th>Thu</th>
+                        <th>Fri</th>
+                        <th>Sat</th>
+                        <th>Sun</th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
      );
 }
 
